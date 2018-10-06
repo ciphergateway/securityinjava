aafd0b4a793d11c504329689d998c684bb1b3aa0
Mike Bai <bxy@ciphergateway.com>
2018-10-06 23:40:53 +0800

    add securityinjava-vertx to gc template
    Signed-off-by: Mike Bai <bxy@ciphergateway.com>

--- support/build-all/com.ciphergateway.build-eclipse-plugin/make.sh
+++ support/build-all/com.ciphergateway.build-eclipse-plugin/make.sh
@@ -24,13 +24,28 @@ extractJar com.ciphergateway.mda.gc-${revision}
 extractJar com.ciphergateway.mda.gp-${revision}
 extractJar com.ciphergateway.mda.libs-${revision}
 extractJar com.ciphergateway.mda.mvm-${revision}
+# back to root dir
+cd ../../../../../../
 
 echo [3/4]copy securityinjava-ssm...
-cd ../../../../../../securityinjava-ssm
+cd securityinjava-ssm
 mvn clean eclipse:clean eclipse:eclipse
+# back to root dir
 cd ..
-
-echo [4/4]extract securityinjava-ssm.jar...
 jar cfM support/build-all/com.ciphergateway.build-eclipse-plugin/target/securityinjava-ssm.jar securityinjava-ssm
 cd support/build-all/com.ciphergateway.build-eclipse-plugin/target/eclipse/plugins/com.ciphergateway.mda.gp-${revision}/t/j1
 jar xfM ../../../../../securityinjava-ssm.jar
+# back to root dir
+cd ../../../../../../../../../
+
+
+echo [4/4]copy securityinjava-vertx...
+cd securityinjava-vertx
+mvn clean eclipse:clean eclipse:eclipse
+# back to root dir
+cd ..
+jar cfM support/build-all/com.ciphergateway.build-eclipse-plugin/target/securityinjava-vertx.jar securityinjava-vertx
+cd support/build-all/com.ciphergateway.build-eclipse-plugin/target/eclipse/plugins/com.ciphergateway.mda.gp-${revision}/t/j2
+jar xfM ../../../../../securityinjava-vertx.jar
+# back to root dir
+cd ../../../../../../../../../
