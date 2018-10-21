72ee0180318a6fbec914ff198c7bf7dd5b239437
Mike Bai <bxy@ciphergateway.com>
2018-10-21 17:40:25 +0800

    clean spring boot properties; fix RmConfig
    Signed-off-by: Mike Bai <bxy@ciphergateway.com>

--- libs/securityinjava-tools/src/main/java/org/quickbundle/config/RmBaseConfigVo.java
+++ libs/securityinjava-tools/src/main/java/org/quickbundle/config/RmBaseConfigVo.java
@@ -9,13 +9,36 @@ public class RmBaseConfigVo {
 		super();
 	}
 	
+	//war包的Home目录
 	private String warHome = System.getProperty("user.home") + "/.ciphergateway/default";
+
+	 /**
+	  * war包的Home目录
+	  * 
+	  * @return the warHome
+     */
+    public String getWarHome() {
+        return warHome;
+    }
+    public void setWarHome(String warHome) {
+        this.warHome = warHome;
+    }
 	
 	/**
 	 * 	<!-- cloud node info collected automatically? or read from rm.xml? -->
 	 * @return 是集群模式？或单机？
 	 */
 	private boolean clusterMode = false;
+	
+    /**
+     * @return 是集群模式？或单机？
+     */
+    public boolean isClusterMode() {
+        return clusterMode;
+    }
+    public void setClusterMode(boolean clusterMode) {
+        this.clusterMode = clusterMode;
+    }
     
     /**
      * 获得默认数据源的数据库类型，默认数据库类型是NULL
@@ -24,300 +47,242 @@ public class RmBaseConfigVo {
 	private String databaseProductName = null;
 	
     /**
+     * @return 获得默认数据源的数据库类型  IGlobalConstants.DATABASE_PRODUCT_NAME_...
+     */
+    public String getDatabaseProductName() {
+        return databaseProductName;
+    }
+    public void setDatabaseProductName(String databaseProductName) {
+        this.databaseProductName = databaseProductName;
+    }	
+	
+    /**
      * 系统是否开发调试状态(性能很低，便利于开发调试。同时sql的?替换输出，日志记录了sql真实数据)
      * 默认不是开发调试状态，是正式运行状态
      */
     private boolean systemDebugMode = true;
+    
+    /**
+     * @return 系统是否开发调试状态(系统综合运行性能较低，优化了应用启动速度。同时sql的?替换输出，日志记录了sql真实数据)
+     */
+    public boolean isSystemDebugMode() {
+        return systemDebugMode;
+    }
+    public void setSystemDebugMode(boolean systemDebugMode) {
+        this.systemDebugMode = systemDebugMode;
+    }
 	
 	/**
 	 * primary key prefix of this cluster node, range: 1000-9222 
 	        本集群节点RmIdFactory产生的主键前缀
 	 */
 	private String shardingPrefix = "1000";
+	
+    /**
+     * @return 本集群节点RmIdFactory产生的主键前缀
+     */
+    public String getShardingPrefix() {
+        return shardingPrefix;
+    }
+    public void setShardingPrefix(String shardingPrefix) {
+        this.shardingPrefix = shardingPrefix;
+    }
 
 	/**
 	 * 系统用户是否唯一登录，同时登录会强制踢出第一个用户
 	 */
 	private boolean userUniqueLogin = true;
+    /**
+     * @return 系统用户是否唯一登录，同时登录会强制踢出第一个用户
+     */
+    public boolean isUserUniqueLogin() {
+        return userUniqueLogin;
+    }
+    public void setUserUniqueLogin(boolean userUniqueLogin) {
+        this.userUniqueLogin = userUniqueLogin;
+    }	
 
 	
 	/**
 	 * 默认的分页条数，默认按15条分页，会被rm.xml/rm/RmJspHelper/pageSize覆盖
 	 */
 	private int defaultPageSize = 15;
+    /**
+     * @return 默认的分页条数，会被rm.xml/rm/RmJspHelper/pageSize覆盖
+     */
+    public int getDefaultPageSize() {
+        return defaultPageSize;
+    }
+    public void setDefaultPageSize(int defaultPageSize) {
+        this.defaultPageSize = defaultPageSize;
+    }	
 	
 	/**
 	 * 默认的批处理条数，用于sql的union all条数
 	 */
 	private int defaultBatchSize = 10;
+    /**
+     * @return the defaultBatchSize
+     */
+    public int getDefaultBatchSize() {
+        return defaultBatchSize;
+    }
+    public void setDefaultBatchSize(int defaultBatchSize) {
+        this.defaultBatchSize = defaultBatchSize;
+    }	
 	
 	/**
 	 * 得到系统描述
 	 */
-	private String appDescription = "QuickBundle System";
+	private String appDescription = "SecurityInJava by CipherGateway";
+    /**
+     * @return 得到系统简短描述
+     */
+    public String getAppDescription() {
+        return appDescription;
+    }
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
+    }	
 	
 	/**
 	 * 是否记录request的执行时间和SQL数量
 	 */
 	private boolean logRequest = true;
+    /**
+     * @return 是否记录request的执行时间和SQL数量
+     */
+    public boolean isLogRequest() {
+        return logRequest;
+    }
+    public void setLogRequest(boolean logRequest) {
+        this.logRequest = logRequest;
+    }
 	
 	/**
 	 * Python的Lib目录不存在时，自动解压缩
 	 */
 	private boolean createPythonLibIfNotExist = true;
+	
+    /**
+     * @return the createPythonLibIfNotExist
+     */
+    public boolean isCreatePythonLibIfNotExist() {
+        return createPythonLibIfNotExist;
+    }
+    public void setCreatePythonLibIfNotExist(boolean createPythonLibIfNotExist) {
+        this.createPythonLibIfNotExist = createPythonLibIfNotExist;
+    }
 
 	/**
 	 * 是否逻辑删除文件
 	 */
 	private boolean logicDeleteFile = false;
 	
+    /**
+     * @return the logicDeleteFile
+     */
+    public boolean isLogicDeleteFile() {
+        return logicDeleteFile;
+    }
+    public void setLogicDeleteFile(boolean logicDeleteFile) {
+        this.logicDeleteFile = logicDeleteFile;
+    }	
+	
 	/**
 	 * 逻辑删除的回收站文件夹
 	 */
 	private String recycleBinFolder = warHome + "/recycle_bin";
+	
+    /**
+     * @return the recycleBinFolder
+     */
+    public String getRecycleBinFolder() {
+        return recycleBinFolder;
+    }
+    public void setRecycleBinFolder(String recycleBinFolder) {
+        this.recycleBinFolder = recycleBinFolder;
+    }
 	
 	/**
 	 * 是否记住当前url列表的在第几行
 	 */
 	private boolean rememberPage = false;
 	
+    /**
+     * 是否记住当前url列表的在第几行
+     * 
+     * @return
+     */
+    public boolean isRememberPage() {
+        return rememberPage;
+    }
+    public void setRememberPage(boolean rememberPage) {
+        this.rememberPage = rememberPage;
+    }	
+	
 	/**
 	 * 默认字体
 	 */
 	private String defaultFont = null;
+	
+    /**
+     * 默认字体
+     * 
+     * @return
+     */
+    public String getDefaultFont() {
+        return defaultFont;
+    }
+    public void setDefaultFont(String defaultFont) {
+        this.defaultFont = defaultFont;
+    }
 	
 	/**
 	 * 最多in多少条记录？
 	 */
 	private int maxSqlInCount = 1000;
 	
+    /**
+     * 最多in多少条记录？
+     * @return
+     */
+    public int getMaxSqlInCount() {
+        return maxSqlInCount;
+    }
+    public void setMaxSqlInCount(int maxSqlInCount) {
+        this.maxSqlInCount = maxSqlInCount;
+    }
+	
 	/**
 	 * 系统默认编码
 	 */
-	private String defaultEncode = "UTF-8";
+    private String defaultEncode = "UTF-8";
+
+    /**
+     * 系统默认编码
+     * @return
+     */
+    public String getDefaultEncode() {
+        return defaultEncode;
+    }
+    public void setDefaultEncode(String defaultEncode) {
+        this.defaultEncode = defaultEncode;
+    }
 	
 	/**
 	 * 默认实数数值的精度
 	 */
 	private int defaultNumberScale = 2;
 
-	/**
-	 * @return the warHome
-	 */
-	public String getWarHome() {
-		return warHome;
-	}
-
-	/**
-	 * @param warHome the warHome to set
-	 */
-	void setWarHome(String warHome) {
-		this.warHome = warHome;
-	}
-
-	/**
-	 * @return 是集群模式？或单机？
-	 */
-	public boolean isClusterMode() {
-		return clusterMode;
-	}
-
-	/**
-	 * @param clusterMode the clusterMode to set
-	 */
-	void setClusterMode(boolean clusterMode) {
-		this.clusterMode = clusterMode;
-	}
-
-	/**
-	 * @return 获得默认数据源的数据库类型  IGlobalConstants.DATABASE_PRODUCT_NAME_...
-	 */
-	public String getDatabaseProductName() {
-		return databaseProductName;
-	}
-
-	/**
-	 * @param databaseProductName the databaseProductName to set
-	 */
-	void setDatabaseProductName(String databaseProductName) {
-		this.databaseProductName = databaseProductName;
-	}
-
-	/**
-	 * @return 系统是否开发调试状态(系统综合运行性能较低，优化了应用启动速度。同时sql的?替换输出，日志记录了sql真实数据)
-	 */
-	public boolean isSystemDebugMode() {
-		return systemDebugMode;
-	}
-
-	/**
-	 * @param systemDebugMode the systemDebugMode to set
-	 */
-	void setSystemDebugMode(boolean systemDebugMode) {
-		this.systemDebugMode = systemDebugMode;
-	}
-
-	/**
-	 * @return 本集群节点RmIdFactory产生的主键前缀
-	 */
-	public String getShardingPrefix() {
-		return shardingPrefix;
-	}
-	/**
-	 * @param shardingPrefix the shardingPrefix to set
-	 */
-	public void setShardingPrefix(String shardingPrefix) {
-		this.shardingPrefix = shardingPrefix;
-	}
-	
-	/**
-	 * @return 系统用户是否唯一登录，同时登录会强制踢出第一个用户
-	 */
-	public boolean isUserUniqueLogin() {
-		return userUniqueLogin;
-	}
-
-	/**
-	 * @param userUniqueLogin the userUniqueLogin to set
-	 */
-	void setUserUniqueLogin(boolean userUniqueLogin) {
-		this.userUniqueLogin = userUniqueLogin;
-	}
-
-	/**
-	 * @return 默认的分页条数，会被rm.xml/rm/RmJspHelper/pageSize覆盖
-	 */
-	public int getDefaultPageSize() {
-		return defaultPageSize;
-	}
-
-	/**
-	 * @param defaultPageSize the defaultPageSize to set
-	 */
-	void setDefaultPageSize(int defaultPageSize) {
-		this.defaultPageSize = defaultPageSize;
-	}
-
-	/**
-	 * @return the defaultBatchSize
-	 */
-	public int getDefaultBatchSize() {
-		return defaultBatchSize;
-	}
-
-	/**
-	 * @param defaultBatchSize the defaultBatchSize to set
-	 */
-	void setDefaultBatchSize(int defaultBatchSize) {
-		this.defaultBatchSize = defaultBatchSize;
-	}
-
-	/**
-	 * @return 得到系统简短描述
-	 */
-	public String getAppDescription() {
-		return appDescription;
-	}
-
-	/**
-	 * @param appDescription the appDescription to set
-	 */
-	void setAppDescription(String appDescription) {
-		this.appDescription = appDescription;
-	}
-
-	/**
-	 * @return 是否记录request的执行时间和SQL数量
-	 */
-	public boolean isLogRequest() {
-		return logRequest;
-	}
-
-	/**
-	 * @param logRequest the logRequest to set
-	 */
-	void setLogRequest(boolean logRequest) {
-		this.logRequest = logRequest;
-	}
-
-	/**
-	 * @return the createPythonLibIfNotExist
-	 */
-	public boolean isCreatePythonLibIfNotExist() {
-		return createPythonLibIfNotExist;
-	}
-
-	/**
-	 * @param createPythonLibIfNotExist the createPythonLibIfNotExist to set
-	 */
-	void setCreatePythonLibIfNotExist(boolean createPythonLibIfNotExist) {
-		this.createPythonLibIfNotExist = createPythonLibIfNotExist;
-	}
-
-	/**
-	 * @return the logicDeleteFile
-	 */
-	public boolean isLogicDeleteFile() {
-		return logicDeleteFile;
-	}
-
-	/**
-	 * @param logicDeleteFile the logicDeleteFile to set
-	 */
-	void setLogicDeleteFile(boolean logicDeleteFile) {
-		this.logicDeleteFile = logicDeleteFile;
-	}
-
-	/**
-	 * @return the recycleBinFolder
-	 */
-	public String getRecycleBinFolder() {
-		return recycleBinFolder;
-	}
-
-	/**
-	 * @param recycleBinFolder the recycleBinFolder to set
-	 */
-	void setRecycleBinFolder(String recycleBinFolder) {
-		this.recycleBinFolder = recycleBinFolder;
-	}
-
-	public boolean isRememberPage() {
-		return rememberPage;
-	}
-
-	void setRememberPage(boolean rememberPage) {
-		this.rememberPage = rememberPage;
-	}
-
-	public String getDefaultFont() {
-		return defaultFont;
-	}
-
-	void setDefaultFont(String defaultFont) {
-		this.defaultFont = defaultFont;
-	}
-	
-	public int getMaxSqlInCount() {
-		return maxSqlInCount;
-	}
-
-	public void setMaxSqlInCount(int maxSqlInCount) {
-		this.maxSqlInCount = maxSqlInCount;
-	}
-
-    public String getDefaultEncode() {
-        return defaultEncode;
-    }
-
-    public void setDefaultEncode(String defaultEncode) {
-        this.defaultEncode = defaultEncode;
-    }
-
+    /**
+     * 默认实数数值的精度
+     * 
+     * @return
+     */
     public int getDefaultNumberScale() {
         return defaultNumberScale;
     }
-
     public void setDefaultNumberScale(int defaultNumberScale) {
         this.defaultNumberScale = defaultNumberScale;
     }
--- securityinjava-ssm/src/main/java/org/quickbundle/config/RmConfigVo.java
+++ securityinjava-ssm/src/main/java/org/quickbundle/config/RmConfigVo.java
@@ -2,8 +2,6 @@ package org.quickbundle.config;
 
 import java.io.File;
 
-import org.quickbundle.config.RmBaseConfigVo;
-
 public class RmConfigVo extends RmBaseConfigVo {
 	
 	private RmBaseConfigVo singleton = null;
@@ -12,8 +10,6 @@ public class RmConfigVo extends RmBaseConfigVo {
 		super();
 		this.singleton = singleton;
 	}
-	
-	private boolean loginValidateVerifyCode = false;
 	
 	/**
 	 * @return the warHome
@@ -212,8 +208,6 @@ public class RmConfigVo extends RmBaseConfigVo {
 	public void setDefaultFont(String defaultFont) {
 		singleton.setDefaultFont(defaultFont);
 	}
-
-	
 	
 	/**
 	 * 默认编码
@@ -222,6 +216,10 @@ public class RmConfigVo extends RmBaseConfigVo {
 		return singleton.getDefaultEncode();
 	}
 	
+    public void setDefaultEncode(String defaultEncode) {
+        singleton.setDefaultEncode(defaultEncode);
+    }
+	
 	/**
 	 * 默认实数数值的精度
 	 */
@@ -229,6 +227,9 @@ public class RmConfigVo extends RmBaseConfigVo {
 		return singleton.getDefaultNumberScale();
 	}
 	
+   public void setDefaultNumberScale(int defaultNumberScale ) {
+        singleton.setDefaultNumberScale(defaultNumberScale);
+    }
 
 
 	/**
@@ -248,10 +249,9 @@ public class RmConfigVo extends RmBaseConfigVo {
 		this.schedulerStart = schedulerStart;
 	}
 	
-	/**
-	 * lazy start scheduler time(second), 0 or -1 ignore this | 延时启动scheduler的秒数
-	 */
+	//lazy start scheduler time(second), 0 or -1 ignore this | 延时启动scheduler的秒数
 	private long schedulerStartLazy;
+
 	/**
 	 * lazy start scheduler time(second), 0 or -1 ignore this | 延时启动scheduler的秒数
 	 * @return
@@ -259,19 +259,16 @@ public class RmConfigVo extends RmBaseConfigVo {
 	public long getSchedulerStartLazy() {
 		return schedulerStartLazy;
 	}
-	/**
-	 * lazy start scheduler time(second), 0 or -1 ignore this | 延时启动scheduler的秒数
-	 * @param schedulerStartLazy
-	 */
 	public void setSchedulerStartLazy(long schedulerStartLazy) {
 		this.schedulerStartLazy = schedulerStartLazy;
 	}
 	
-	
-
-	
+	   
+    //是否开启登录图片校验
+    private boolean loginValidateVerifyCode = false;
+    
 	/**
-	 * 
+	 * 登录时是否有校验码
 	 * 
 	 * @return
 	 */
@@ -279,90 +276,184 @@ public class RmConfigVo extends RmBaseConfigVo {
         return loginValidateVerifyCode;
     }
 
-    /**
-     * 设置登录时是否有校验码
-     * @param loginValidateVerifyCode
-     */
     public void setLoginValidateVerifyCode(boolean loginValidateVerifyCode) {
         this.loginValidateVerifyCode = loginValidateVerifyCode;
     }
-
-    //未加入rm.xml文件的配置
-    /**
-     * @return 系统缓存检查周期
-     */
-    public long getCacheCheckInterval() {
-        return 1000 * 2;
-    }
+    
+    //节点心跳间隔，单位ms
+    private long nodeHeartbeatInterval = 1000 * 60;
     
     /**
-     * @return 是否全局监控
-     */
-    public boolean isGlobalMonitor() {
-        return true;
-    }
-    
-    /**
-     * @return 系统缓存刷新周期
-     */
-    public long getCacheFlushInterval() {
-        return 1000 * 60 * 5;
-    }
-    
-    /**
-     * @return 节点心跳间隔
+     * 节点心跳间隔，单位ms
+     * 
+     * @return
      */
     public long getNodeHeartbeatInterval() {
-        return 1000 * 60;
+        return nodeHeartbeatInterval;
     }
+
+    public void setNodeHeartbeatInterval(long nodeHeartbeatInterval) {
+        this.nodeHeartbeatInterval = nodeHeartbeatInterval;
+    }
+
+    //系统缓存检查周期
+    private long cacheCheckInterval = 1000 * 2;
+
+    /**
+     * 系统缓存检查周期
+     * 
+     * @return
+     */
+    public long getCacheCheckInterval() {
+        return cacheCheckInterval;
+    }
+
+    public void setCacheCheckInterval(long cacheCheckInterval) {
+        this.cacheCheckInterval = cacheCheckInterval;
+    }
+
+    //是否全局监控
+    private boolean globalMonitor = true;
+    
+    /**
+     * 是否全局监控
+     * 
+     * @return
+     */
+    public boolean isGlobalMonitor() {
+        return globalMonitor;
+    }
+
+    public void setGlobalMonitor(boolean globalMonitor) {
+        this.globalMonitor = globalMonitor;
+    }
+
+    //系统缓存刷新周期
+    private long cacheFlushInterval = 1000 * 60 * 5;
+
+    /**
+     * 系统缓存刷新周期
+     * 
+     * @return
+     */
+    public long getCacheFlushInterval() {
+        return cacheFlushInterval;
+    }
+
+    public void setCacheFlushInterval(long cacheFlushInterval) {
+        this.cacheFlushInterval = cacheFlushInterval;
+    }
+
+    //批处理sql的最大记录日志数量
+    private long maxLogSqlBatchSize = 100;
     
     /**
      * 批处理sql的最大记录日志数量
+     * 
+     * @return
      */
-    public int getMaxLogSqlBatchSize() {
-        return 100;
+    public long getMaxLogSqlBatchSize() {
+        return maxLogSqlBatchSize;
     }
+
+    public void setMaxLogSqlBatchSize(long maxLogSqlBatchSize) {
+        this.maxLogSqlBatchSize = maxLogSqlBatchSize;
+    }
+
+    //系统用户登录是否DEMO状态(不校验用户数据库)
+    private boolean userDemoMode = true;
     
     /**
      * 系统用户登录是否DEMO状态(不校验用户数据库)
+     * 
+     * @return
      */
     public boolean isUserDemoMode() {
-        return true;
+        return userDemoMode;
     }
+
+    public void setUserDemoMode(boolean userDemoMode) {
+        this.userDemoMode = userDemoMode;
+    }
+
+    //是否给insert和update的sql语句自动加ts
+    private boolean sqlUpdateAutoAppendTs = false;
     
     /**
      * 是否给insert和update的sql语句自动加ts
+     * 
+     * @return
      */
     public boolean isSqlUpdateAutoAppendTs() {
-        return false;
+        return sqlUpdateAutoAppendTs;
     }
+
+    public void setSqlUpdateAutoAppendTs(boolean sqlUpdateAutoAppendTs) {
+        this.sqlUpdateAutoAppendTs = sqlUpdateAutoAppendTs;
+    }
+
+    //默认的临时文件夹
+    private File defaultTempDir = new File(System.getProperty("java.io.tmpdir") + File.separator + "securityinjava");
     
     /**
      * 默认的临时文件夹
+     * 
+     * @return
      */
     public File getDefaultTempDir() {
-        return new File(System.getProperty("java.io.tmpdir") + File.separator + "securityinjava");
+        return defaultTempDir;
     }
-    
+
+    public void setDefaultTempDir(File defaultTempDir) {
+        this.defaultTempDir = defaultTempDir;
+    }
+
+    //默认的树形编码起始值，适用于简单的纯数字树，每个节点下最多有900个子节点
+    private String defaultTreeCodeFirst = "100";
+
     /**
      * 默认的树形编码起始值，适用于简单的纯数字树，每个节点下最多有900个子节点
+     * 
+     * @return
      */
     public String getDefaultTreeCodeFirst() {
-        return "100";
+        return defaultTreeCodeFirst;
     }
+
+    public void setDefaultTreeCodeFirst(String defaultTreeCodeFirst) {
+        this.defaultTreeCodeFirst = defaultTreeCodeFirst;
+    }
+
+    //指定最大循环次数，防止死循环
+    private int maxCircleCount = 10000;
     
     /**
      * 指定最大循环次数，防止死循环
-     */
-    public int getMaxCircleCount() {
-        return 10000;
-    }
-    
-    /**
-     * 定义单实例全局缓存的最大容量，防止溢出攻击，如公开的url列表
+     * 
      * @return
      */
-    public int getMaxCacheSize() {
-        return 10000;
+    public int getMaxCircleCount() {
+        return maxCircleCount;
     }
+
+    public void setMaxCircleCount(int maxCircleCount) {
+        this.maxCircleCount = maxCircleCount;
+    }
+
+    //定义单实例全局缓存的最大容量，防止溢出攻击，如公开的url列表
+    private int maxCacheSize = 10000;
+
+    public int getMaxCacheSize() {
+        return maxCacheSize;
+    }
+
+    /**
+     * 定义单实例全局缓存的最大容量，防止溢出攻击，如公开的url列表
+     * 
+     * @param maxCacheSize
+     */
+    public void setMaxCacheSize(int maxCacheSize) {
+        this.maxCacheSize = maxCacheSize;
+    }
+    
 }
